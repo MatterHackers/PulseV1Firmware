19952209485a7f842c87030b82a563cc59b3f599
Lars Brubaker <larsbrubaker@gmail.com>
2017-01-19 17:49:54 -0800

    Update build area, software endstops, hardware endstops, disable unused pins

--- Marlin/pins.h
+++ Marlin/pins.h
@@ -597,18 +597,18 @@
     #define X_DIR_PIN          55
     #define X_ENABLE_PIN       38
     #define X_MIN_PIN           3
-    #define X_MAX_PIN           2
+    #define X_MAX_PIN           -1 // 2
 
     #define Y_STEP_PIN         60
     #define Y_DIR_PIN          61
     #define Y_ENABLE_PIN       56
     #define Y_MIN_PIN          14
-    #define Y_MAX_PIN          15
+    #define Y_MAX_PIN          -1 // 15
 
     #define Z_STEP_PIN         46
     #define Z_DIR_PIN          48
     #define Z_ENABLE_PIN       62
-    #define Z_MIN_PIN          18
+    #define Z_MIN_PIN          -1 // 18
     #define Z_MAX_PIN          19
 
     #define Y2_STEP_PIN        36
